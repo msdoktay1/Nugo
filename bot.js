0b6748b536a4f1734136845e7114296f4a8a5cca
msdoktay1 <galatalioktay@gmail.com>
2024-06-11 13:17:08 +0300

    s

--- bot.js
+++ bot.js
@@ -4,21 +4,34 @@ const BOT_TOKEN = '7442648171:AAGL_JtyXx6XGXNCKnOIcJF-ITXud8alFTo';
 const bot = new Telegraf(BOT_TOKEN);
 
 // Oyunun short_name'ini ve URL'sini tanımlayın
-const GAME_SHORT_NAME = 'Nugo';
-const GAME_URL = 'https://msdoktay1.github.io/Nugo/';
+const GAME_SHORT_NAME = 'nugocoin';
+const GAME_URL = 'https://msdoktay1.github.io/Nugo/index.html?';
 
 bot.command('start', (ctx) => {
-  // Oyunu kullanıcıya gönder
-  ctx.replyWithGame(GAME_SHORT_NAME);
+    // Oyunu kullanıcıya gönder
+    ctx.replyWithGame(GAME_SHORT_NAME);
+});
+bot.command('start', (ctx) => {
+    ctx.reply('Oyunu başlatmak için aşağıdaki butona tıklayın:', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Nugo Oyna', url: 'https://msdoktay1.github.io/Nugo/' }]
+            ]
+        }
+    });
 });
 
+
 // CallbackQuery'yi yakala ve oyunu başlat
-bot.on('callback_query', (callbackQuery) => {
-  const gameShortName = callbackQuery.game_short_name;
-  if (gameShortName === GAME_SHORT_NAME) {
-    // Oyun URL'sine yönlendir
-    bot.answerCallbackQuery(callbackQuery.id, { url: GAME_URL });
-  }
+bot.on('callback_query', (ctx) => {
+    const callbackQueryId = ctx.callbackQuery.id;
+    const gameShortName = ctx.callbackQuery.game_short_name;
+    console.log(gameShortName);
+    console.log(GAME_SHORT_NAME);
+    if (gameShortName === GAME_SHORT_NAME) {
+        // Oyun URL'sine yönlendir
+        ctx.answerCbQuery(callbackQueryId, undefined, false, { url: GAME_URL });
+    }
 });
 
 // Botu başlat
