1954e7da7acf44776374a3354d5638928970ae96
msdoktay1 <galatalioktay@gmail.com>
2024-06-12 03:04:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { TelegramBot } from 'node-telegram-bot-api';
+import { TelegramBot } from '.node-telegram-bot-api';
 const token = '7442648171:AAGL_JtyXx6XGXNCKnOIcJF-ITXud8alFTo'; // Gerçek tokeninizi buraya girin.
 const telegramBot = new TelegramBot(token, { polling: true });
 
